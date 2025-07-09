0e704bcf429bd66133b10f78ab2d1787c8c08650
Jeff Peralta <jaydocperalta@gmail.com>
2025-07-09 14:48:44 +1000

    Init

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hello {{ counter }}! {{ name }}
+  </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+  
+  defineProps({
+    name: String,
+  });
+
+  const counter = ref(0)
+  setInterval(() => {
+    counter.value++
+  }, 1000);
+</script>
+
+<script>
+  import MainLayout from '../../Layouts/MainLayout.vue';
+  export default {
+    layout: MainLayout
+  }
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Show Page</div>
+</template>
+
+<script setup></script>
+
+<script>
+  import MainLayout from '../../Layouts/MainLayout.vue';
+  export default {
+    layout: MainLayout
+  }
+</script>
