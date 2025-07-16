602faed119450cf57e361bb89f7c2c16b32577c8
Jeff Peralta <jaydocperalta@gmail.com>
2025-07-16 15:55:51 +1000

    Form validation during create

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -4,41 +4,49 @@
       <div>
         <label>Beds</label>
         <input type="text" v-model.number="form.beds" />
+        <div v-if="form.errors.beds" class="error">{{ form.errors.beds }}</div>
       </div>
 
       <div>
         <label>Baths</label>
         <input type="text" v-model.number="form.baths"/>
+        <div v-if="form.errors.baths" class="error">{{ form.errors.baths }}</div>
       </div>
 
       <div>
         <label>Area</label>
         <input type="text" v-model.number="form.area"/>
+        <div v-if="form.errors.area" class="error">{{ form.errors.area }}</div>
       </div>
 
       <div>
         <label>City</label>
         <input type="text" v-model="form.city"/>
+        <div v-if="form.errors.city" class="error">{{ form.errors.city }}</div>
       </div>
 
       <div>
         <label>Post Code</label>
         <input type="text" v-model="form.code"/>
+        <div v-if="form.errors.code" class="error">{{ form.errors.code }}</div>
       </div>
 
       <div>
         <label>Street</label>
         <input type="text" v-model="form.street"/>
+        <div v-if="form.errors.street" class="error">{{ form.errors.street }}</div>
       </div>
 
       <div>
         <label>Street Nr</label>
         <input type="text" v-model="form.street_nr"/>
+        <div v-if="form.errors.street_nr" class="error">{{ form.errors.street_nr }}</div>
       </div>
 
       <div>
         <label>Price</label>
         <input type="text" v-model.number="form.price"/>
+        <div v-if="form.errors.price" class="error">{{ form.errors.price }}</div>
       </div>
 
       <div>
@@ -73,5 +81,10 @@ label {
 
 div {
   padding: 2px
+}
+
+.error {
+    color: red;
+    margin-bottom: 10px;
 }
 </style>
