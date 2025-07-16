58434a618172822d42ff620bdff5d92505aa1b0b
Jeff Peralta <jaydocperalta@gmail.com>
2025-07-16 15:37:59 +1000

    Create Listing + flash message handling

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -6,17 +6,30 @@
       <Link href="/show">Show Page</Link> &nbsp;
       <Link href="/listing">Listing Page</Link>
     </nav>
-    <div>{{ timer }}</div>
+    <!--<div>{{ timer }}</div>-->
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
     <slot />
   </div>
 </template>
 
 <script setup>
-    import { Link } from '@inertiajs/vue3'
-    import { ref } from 'vue'
+    import { computed } from 'vue'
+    import { Link, usePage } from '@inertiajs/vue3'
+    // import { ref } from 'vue'
+    // const timer = ref(0)
+    // setInterval(() => {
+    //  timer.value++
+    //}, 1000)
 
-    const timer = ref(0)
-    setInterval(() => {
-      timer.value++
-    }, 1000)
+    const page = usePage()
+    const flashSuccess = computed(() => page.props.flash?.success ?? "")
 </script>
+
+<style scope>
+    .success {
+        background-color: green;
+        color: white;
+    }
+</style>
