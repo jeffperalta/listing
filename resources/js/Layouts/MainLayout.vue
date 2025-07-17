b5f4cd4787d96558cbb66d348847f4acc5503634
Jeff Peralta <jaydocperalta@gmail.com>
2025-07-17 17:07:15 +1000

    Setup tailwind css and apply design to the layout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,27 @@
 <!-- resources/js/Layouts/MainLayout.vue -->
 <template>
   <div>
-    <nav class="main-nav">
-      <Link :href="route('listing.index')">Listings</Link>
-      <Link :href="route('listing.create')">New Listing</Link>
-    </nav>
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
-    <slot />
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.index')">Listings</Link>
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listing.index')">Laravel</Link>
+                </div>
+                <div class="text-lg font-medium">
+                    <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New Listing</Link>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess" class="mb-4 border-2 rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot />
+    </main>
   </div>
 </template>
 
@@ -18,24 +31,3 @@
     const page = usePage()
     const flashSuccess = computed(() => page.props.flash?.success ?? "")
 </script>
-
-<style scope>
-    .success {
-        padding: 10px;
-        border: 2px solid darkgreen;
-        background-color: green;
-        color: white;
-        border-radius: 5px;
-        margin: 10px 5px;
-    }
-
-    .main-nav {
-        min-height: 30px;
-        a {
-            margin: 2px;
-            border: 1px solid black;
-            border-radius: 5px 5px 0 0;
-            padding: 5px;
-        }
-    }
-</style>
