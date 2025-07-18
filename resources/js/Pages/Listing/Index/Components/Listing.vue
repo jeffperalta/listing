ddba5137bf4d683cd4d2624c95884548db1f6798
Jeff Peralta <jaydocperalta@gmail.com>
2025-07-18 15:56:32 +1000

    New component Listing showing the monthly payment

---
+++ resources/js/Pages/Listing/Index/Components/Listing.vue
@@ -0,0 +1,47 @@
+<template>
+  <Box :key="listing.id">
+      <div>
+          <Link :href="route('listing.show', {listing: listing.id})">
+              <div class="flex flex-row gap-2 items-center">
+                  <Price :price="listing.price" class="text-2xl font-bold text-gray-600" />
+                  <div class="text-xs text-gray-500">
+                      <Price :price="monthlyPayment" /> pm
+                  </div>
+              </div>
+              <ListingSpace :listing="listing" class="text-lg"/>
+              <ListingAddress :listing="listing" class="text-gray-500" />
+          </Link>
+      </div>
+      <div>
+          [<Link :href="route('listing.edit', {listing: listing.id})">
+              Edit
+          </Link>]
+      </div>
+      <div>
+          [<Link :href="route('listing.destroy', {listing: listing.id})" method="DELETE">
+              Delete
+          </Link>]
+      </div>
+  </Box>
+</template>
+
+<script setup>
+    import { useMonthlyPayment } from '@/Compasables/useMonthlyPayment';
+    import { Link } from '@inertiajs/vue3';
+    import ListingAddress from '@/Components/ListingAddress.vue'
+    import Box from '@/Components/UI/Box.vue';
+    import ListingSpace from '@/Components/ListingSpace.vue';
+    import Price from '@/Components/Price.vue';
+
+
+    const props = defineProps({
+      listing: Object
+    })
+
+    const { monthlyPayment } = useMonthlyPayment(
+        props.listing.price,
+        2.5,
+        25
+    );
+
+</script>
