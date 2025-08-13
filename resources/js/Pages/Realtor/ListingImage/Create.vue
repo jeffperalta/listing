d37432eade17e5973a452651d13a634abc732b2e
Jeff Peralta <jaydocperalta@gmail.com>
2025-08-13 17:12:35 +1000

    Listing Image upload

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,22 +1,49 @@
 <template>
-  <form 
-    method="POST" 
-    :action="route('realtor.listing.image.store', {listing: listing.id})"
-    enctype="multipart/form-data"
-  >
-    <input type="hidden" name="_token" :value="csrf">
-    <input type="file" class="input" multiple name="images[]" />
-    <button type="submit">Send</button>
-  </form>
+  <Box>
+    <template #header>Upload New Images</template>
+    <form @submit.prevent="upload">
+      <div class="mb-4">
+        <input type="hidden" name="_token" :value="csrf">
+        <input type="file" class="input" multiple @input="addFiles" />
+      </div>
+      <div class="flex gap-2">
+        <button type="submit" class="btn-outline">Upload</button>
+        <button type="reset" @click="reset" class="btn-outline">Reset</button>
+      </div>
+    </form>
+  </Box>
 </template>
 
 <script setup>
   import { ref } from 'vue';
+  import Box from '@/Components/UI/Box.vue';
+  import { useForm } from '@inertiajs/vue3';
+  const csrf = ref(document.querySelector('meta[name="csrf-token"]').getAttribute('content'))
 
-  defineProps({
+  const props = defineProps({
     listing: Object
   });
 
-  const csrf = ref(document.querySelector('meta[name="csrf-token"]').getAttribute('content'))
+  const form = useForm({
+    _token: csrf,
+    images: []
+  })
 
+  const upload = () => {
+    form.post(
+      route('realtor.listing.image.store', {listing: props.listing.id}),
+      { onSuccess: reset }
+    );
+  }
+
+  const addFiles = (event) => {
+    for(const image of event.target.files) {
+      form.images.push(image);
+    }
+  }
+
+  const reset = () => {
+    form.reset('images');
+  }
+  
 </script>
